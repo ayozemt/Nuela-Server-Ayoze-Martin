663bd83f9ac3f39bf7454d8580410e5814cc18e9
ayozemt <ayozemt@gmail.com>
2024-04-01 14:02:36 +0200

    Add timestamps to Subject model

--- models/Subject.model.js
+++ models/Subject.model.js
@@ -1,42 +1,50 @@
 const { Schema, model } = require("mongoose");
 
-const subjectSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Nombre de la asignatura necesario"],
-    trim: true,
+const subjectSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Nombre de la asignatura necesario"],
+      trim: true,
+    },
+    type: {
+      type: String,
+      enum: ["Obligatoria", "Optativa"],
+      required: [true, "El tipo de asignatura es necesario"],
+    },
+    grade: {
+      type: String,
+      enum: [
+        "1 de ESO",
+        "2 de ESO",
+        "3 de ESO",
+        "4 de ESO",
+        "1 de Bachillerato",
+        "2 de Bachillerato",
+      ],
+      required: [true, "Curso necesario"],
+    },
+    class: {
+      type: String,
+      enum: ["A", "B", "C", "D"],
+      required: [true, "El grupo es necesario"],
+    },
+    hours: {
+      type: Number,
+      required: [true, "El número de horas es necesario"],
+      min: 0,
+    },
+    group: {
+      type: String,
+    },
+    // Para esta prueba técnica no se va a utilizar, pero en la vida real supongo que las asignaturas irían referenciadas a un profesor
+    // teacher: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: "Teacher",
+    // },
   },
-  type: {
-    type: String,
-    enum: ["Obligatoria", "Optativa"],
-    required: [true, "El tipo de asignatura es necesario"],
-  },
-  grade: {
-    type: String,
-    enum: [
-      "1 de ESO",
-      "2 de ESO",
-      "3 de ESO",
-      "4 de ESO",
-      "1 de Bachillerato",
-      "2 de Bachillerato",
-    ],
-    required: [true, "Curso necesario"],
-  },
-  class: {
-    type: String,
-    enum: ["A", "B", "C", "D"],
-    required: [true, "El grupo es necesario"],
-  },
-  hours: {
-    type: Number,
-    required: [true, "El número de horas es necesario"],
-    min: 0,
-  },
-  group: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 subjectSchema.pre("save", function (next) {
   this.group = `${this.grade} - Grupo ${this.class}`;
