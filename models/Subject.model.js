2c72c124fdd48b45d4d91b0be02114ffd7d45f7a
ayozemt <ayozemt@gmail.com>
2024-04-01 12:16:36 +0200

    Create Subject model

---
+++ models/Subject.model.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+
+const subjectSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Nombre de la asignatura necesario"],
+    trim: true,
+  },
+  type: {
+    type: String,
+    enum: ["Obligatoria", "Optativa"],
+    required: [true, "El tipo de asignatura es necesario"],
+  },
+  grade: {
+    type: String,
+    enum: [
+      "1 de ESO",
+      "2 de ESO",
+      "3 de ESO",
+      "4 de ESO",
+      "1 de Bachillerato",
+      "2 de Bachillerato",
+    ],
+    required: [true, "Curso necesario"],
+  },
+  class: {
+    type: String,
+    enum: ["A", "B", "C", "D"],
+    required: [true, "El grupo es necesario"],
+  },
+  hours: {
+    type: Number,
+    required: [true, "El número de horas es necesario"],
+    min: 0,
+  },
+  group: {
+    type: String,
+  },
+});
+
+subjectSchema.pre("save", function (next) {
+  this.group = `${this.grade} - Grupo ${this.class}`;
+  next();
+});
+
+const Subject = model("Subject", subjectSchema);
+
+module.exports = Subject;
